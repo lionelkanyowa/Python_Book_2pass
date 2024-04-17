9afc6fc76b6ae998f62e7e1dd3d05279b5f81f20
Lionel Kanyowa <lionelkanyowa@gmail.com>
2024-04-17 16:00:51 -0400

    chore:input/output exercises 2nd Pass

---
+++ input-output2/q1_in-out2.py
@@ -0,0 +1,8 @@
+# Q.1 Write a program named greeter.py. The program should ask for your name, then output Hello, NAME! where NAME is
+# the name you entered:
+
+# What is your name? Sue
+# Hello, Sue!
+
+name = input("What is your name? ")
+print(f'Hello, {name}!')
---
+++ input-output2/q3_in-out2.py
@@ -0,0 +1,10 @@
+# Q.3 Write a program named that asks the user to enter their age, then calculates and reports the future age
+# 10, 20, 30, and 40 years from now. Here's the output for someone who is 27 years old.
+
+age = int(input('How old are you? '))
+
+print(f'You are {age} old.')
+print(f'in 10 years, you will be {age + 10} years old')
+print(f'in 20 years, you will be {age + 20} years old')
+print(f'in 30 years, you will be {age + 30} years old')
+print(f'in 40 years, you will be {age + 40} years old')
